542f117e15ae389ac153069ba9819452b190c42f
g-akca <gok.akca66@gmail.com>
2026-03-13 01:14:27 +0300

    Move CSS variables to Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+
+        blue: {
+          950: "#0D192C",
+          900: "#15263F",
+          800: "#2E405A",
+          500: "#8BACD9",
+        },
+
+        cyan: {
+          400: "#00FFF8",
+        },
+      },
+
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+
+      fontSize: {
+        base: "18px",
+      },
+
+      lineHeight: {
+        base: "125%",
+      },
+    },
+  },
+};
