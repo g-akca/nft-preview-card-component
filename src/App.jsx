f3b706620084b9bdf46fc9da43f579371e824062
g-akca <gok.akca66@gmail.com>
2026-03-13 02:51:55 +0300

    Add Card styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Card from "./components/Card.jsx";
 
 function App() {
   return (
-    <main className="bg-blue-950 text-blue-500 min-h-screen">
+    <main className="bg-blue-950 text-blue-500 min-h-screen flex justify-center items-center p-3 text-base leading-base">
       <Card />
     </main>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,28 +6,29 @@ import viewIcon from "/assets/icon-view.svg";
 
 function Card() {
   return (
-    <div className="max-w-87.5 bg-blue-900 p-6">
-      <img src={equilibriumImg} alt="A colorful cube illustration" />
-      <div>
-        <h2>Equilibrium #3429</h2>
-        <p>Our Equilibrium collection promotes balance and calm.</p>
+    <div className="max-w-87.5 bg-blue-900 p-6 rounded-[15px]">
+      <img src={equilibriumImg} alt="A colorful cube illustration" className="rounded-lg mb-6" />
 
-        <div>
-          <div>
-            <img src={ethereumIcon} alt="Blue crystal icon" />
-            <p>0.041 ETH</p>
+      <div>
+        <h2 className="mb-4 text-white text-[22px] font-semibold">Equilibrium #3429</h2>
+        <p className="font-light leading-6.5">Our Equilibrium collection promotes balance and calm.</p>
+
+        <div className="py-6 flex justify-between items-center border-b border-blue-800">
+          <div className="flex items-center gap-2">
+            <img src={ethereumIcon} alt="Blue crystal icon" className="h-4.5" />
+            <p className="text-cyan-400 font-semibold text-[16px]">0.041 ETH</p>
           </div>
 
-          <div>
-            <img src={clockIcon} alt="Clock icon" />
-            <p>3 days left</p>
+          <div className="flex items-center gap-2">
+            <img src={clockIcon} alt="Clock icon" className="h-4" />
+            <p className="text-[16px]">3 days left</p>
           </div>
         </div>
 
-        <div>
-          <img src={avatarImg} alt="Avatar photo of a woman" />
-          <p>Creation of</p>
-          <p>Jules Wyvern</p>
+        <div className="mt-6 flex items-center text-[16px]">
+          <img src={avatarImg} alt="Avatar photo of a woman" className="h-8 border border-white rounded-full mr-4" />
+          <p className="mr-2 text-blue-500">Creation of</p>
+          <p className="text-white">Jules Wyvern</p>
         </div>
       </div>
     </div>
