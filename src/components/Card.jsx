f2b0ab1affe8f7da907e1e1e0fc616af07accbcc
g-akca <gok.akca66@gmail.com>
2026-03-13 02:59:18 +0300

    Add hover styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,10 +7,16 @@ import viewIcon from "/assets/icon-view.svg";
 function Card() {
   return (
     <div className="max-w-87.5 bg-blue-900 p-6 rounded-[15px]">
-      <img src={equilibriumImg} alt="A colorful cube illustration" className="rounded-lg mb-6" />
+      <div className="relative group mb-6">
+        <img src={equilibriumImg} alt="A colorful cube illustration" className="rounded-lg" />
+
+        <div className="absolute inset-0 bg-cyan-400/50 opacity-0 group-hover:opacity-100 flex items-center justify-center rounded-lg cursor-pointer">
+          <img src={viewIcon} alt="Eye icon" />
+        </div>
+      </div>
 
       <div>
-        <h2 className="mb-4 text-white text-[22px] font-semibold">Equilibrium #3429</h2>
+        <h2 className="mb-4 text-white text-[22px] font-semibold hover:text-cyan-400 cursor-pointer">Equilibrium #3429</h2>
         <p className="font-light leading-6.5">Our Equilibrium collection promotes balance and calm.</p>
 
         <div className="py-6 flex justify-between items-center border-b border-blue-800">
@@ -28,7 +34,7 @@ function Card() {
         <div className="mt-6 flex items-center text-[16px]">
           <img src={avatarImg} alt="Avatar photo of a woman" className="h-8 border border-white rounded-full mr-4" />
           <p className="mr-2 text-blue-500">Creation of</p>
-          <p className="text-white">Jules Wyvern</p>
+          <p className="text-white hover:text-cyan-400 cursor-pointer">Jules Wyvern</p>
         </div>
       </div>
     </div>
