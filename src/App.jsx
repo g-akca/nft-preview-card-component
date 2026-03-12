ccae179b48f033ba690c00dad4811bdbd5efd429
g-akca <gok.akca66@gmail.com>
2026-03-13 01:51:52 +0300

    Add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
+import Card from "./components/Card.jsx";
 
 function App() {
   return (
-    <>
-    </>
+    <main className="bg-blue-950 text-blue-500 min-h-screen">
+      <Card />
+    </main>
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card() {
+  return (
+    <div className="max-w-87.5">
+    </div>
+  )
+}
+
+export default Card;
